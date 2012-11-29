2e729d4979ee059244fa324fe263300a2533ac20
Walter Galante <devilicecream@do>
2012-11-29 20:10:47 +0100

    Added possibility to block controllers

--- tgext/pluggable/adapt_controllers.py
+++ tgext/pluggable/adapt_controllers.py
@@ -1,4 +1,9 @@
+from pylons.controllers.util import abort
 from tg.wsgiapp import TGApp
+import tg
+
+def block_function(*args, **kw):
+    abort(404)
 
 class ControllersAdapter(object):
     def __init__(self, config, controllers, options):
@@ -11,5 +16,17 @@ class ControllersAdapter(object):
         app_id = self.options['appid']
 
         setattr(root_controller, app_id, self.controllers.RootController())
+
+        print "Looking for blocked controllers in %s..." % app_id
+        blocked_controllers = self.options.get('block_controllers',[])
+
+        for controller in blocked_controllers:
+            for name, cont in self.controllers.root.__dict__.items():
+                if isinstance(cont, tg.controllers.decoratedcontroller._DecoratedControllerMeta):
+                    for n, c in cont.__dict__.items():
+                        if hasattr(c, 'decoration') and c.decoration.exposed:
+                            if app_id == controller.get('path') and n == controller.get('name'):
+                                print "Blocked '%s' in '%s'" % (n, app_id)
+                                c.decoration.hooks['before_validate'] = [block_function]
 
         return app
