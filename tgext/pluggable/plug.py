7c460f8fda089cca86939271548ff00ce8565a4d
Alessandro Molina <alessandro.molina@axant.it>
2017-02-22 23:48:53 +0100

    Improve helpers support for TG2.3.10 allowing unimported helpers

--- tgext/pluggable/plug.py
+++ tgext/pluggable/plug.py
@@ -34,20 +34,31 @@ def init_pluggables(app_config):
         def enable_statics_middleware(app):
             return PluggedStaticsMiddleware(app, plugged)
         app_config.register_hook('after_config', enable_statics_middleware)
+        
         if app_config.get('i18n.enabled'):
             app_config.register_hook('controller_wrapper',
                                      pluggable_translations_wrapper)
 
+        app_config._pluggable_partials_cache = {}
+
         #Inject call_partial helper if application has helpers
+        def enable_pluggable_helpers(app_helpers):
+            if not app_helpers:
+                return
+            app_helpers.call_partial = call_partial
+            app_helpers.plug_url = plug_url
+            
         try:
             app_helpers = app_config.package.lib.helpers
         except:
-            app_helpers = None
-
-        if app_helpers:
-            app_config._pluggable_partials_cache = {}
-            app_helpers.call_partial = call_partial
-            app_helpers.plug_url = plug_url
+            # helpers is not an attribute of lib, use what the configured app usees.
+            app_config.register_hook(
+                'configure_new_app',
+                lambda app: enable_pluggable_helpers(app.config.get('helpers'))
+            )
+        else:
+            # Backward compatible for versions that didn't have configure_new_app
+            enable_pluggable_helpers(app_helpers)
 
     return plugged
 
@@ -94,23 +105,20 @@ class ApplicationPlugger(object):
             models_adapter.init_model()
 
         if hasattr(module, 'helpers') and options.get('plug_helpers', True):
+            enable_global_helpers = options.get('global_helpers', False)
             try:
                 app_helpers = app_config.package.lib.helpers
             except:
-                app_helpers = None
-
-            if app_helpers:
-                setattr(app_helpers, module_name, module.helpers)
-
-                if options.get('global_helpers', False):
-                    for name, impl in inspect.getmembers(module.helpers):
-                        if name.startswith('_'):
-                            continue
-
-                        if not hasattr(app_helpers, name):
-                            setattr(app_helpers, name, impl)
-                        else:
-                            log.warning('%s helper already existing, skipping it' % name)
+                app_config.register_hook(
+                    'configure_new_app',
+                    lambda app: self._plug_helpers(app.config.get('helpers'),
+                                                   enable_global_helpers,
+                                                   module_name,
+                                                   module)
+                )
+            else:
+                # Backward compatible for versions that didn't have configure_new_app
+                self._plug_helpers(app_helpers, enable_global_helpers, module_name, module)
 
         if hasattr(module, 'controllers') and options.get('plug_controller', True):
             controllers_adapter = ControllersAdapter(app_config, module.controllers, options)
@@ -125,7 +133,23 @@ class ApplicationPlugger(object):
             statics_adapter = StaticsAdapter(app_config, module, options)
             statics_adapter.register_statics(module_name, self.plugged)
 
+    def _plug_helpers(self, app_helpers, enable_global_helpers, module_name, module):
+        if app_helpers is None:
+            return
+        
+        setattr(app_helpers, module_name, module.helpers)
 
+        if enable_global_helpers:
+            for name, impl in inspect.getmembers(module.helpers):
+                if name.startswith('_'):
+                    continue
+
+                if not hasattr(app_helpers, name):
+                    setattr(app_helpers, name, impl)
+                else:
+                    log.warning('%s helper already existing, skipping it' % name)
+
+            
 def plug(app_config, module_name, appid=None, **kwargs):
     plugged = init_pluggables(app_config)
 
